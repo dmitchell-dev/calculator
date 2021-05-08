57a70aff1d850e45110dcf38119ffc386339cf8e
David Mitchell <dave@dmitchell.dev>
2021-05-08 21:32:35 +0100

    Add urls, views and templates

--- calculator/views.py
+++ calculator/views.py
@@ -1,3 +1,32 @@
+from django.views.generic import TemplateView
 from django.shortcuts import render
+from calculator.forms import HomeForm
+
 
 # Create your views here.
+class HomePage(TemplateView):
+    template_name = 'calculator/home.html'
+
+    def get(self, request, *args, **kwargs):
+        form = HomeForm()
+        return render(request,self.template_name, {'form':form})
+
+    def post(self,request):
+        form = HomeForm(request.POST)
+        if form.is_valid():
+            text = form.cleaned_data['num1']
+            text2 = form.cleaned_data['num2']
+            if 'add' in request.POST:
+                result = text + text2
+            elif 'sub' in request.POST:
+                result = text - text2
+            elif 'mul' in request.POST:
+                result = text * text2
+            elif 'div' in request.POST:
+                result = text / text2
+
+            form = HomeForm()
+            #return redirect ('home:home')
+
+        args = {'form': form , 'result': result}
+        return render(request, self.template_name, args )
