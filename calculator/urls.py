cdeaa3cc1138b00c8cb1974a6376b3f97f41e2d2
David Mitchell <dave@dmitchell.dev>
2021-05-09 13:03:12 +0100

    Update url and namespaces

--- calculator/urls.py
+++ calculator/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import HomePage
 
+app_name='calculator'
+
 urlpatterns = [
-    path('', HomePage.as_view()),
+    path('', HomePage.as_view(), name="homepage"),
 ]
